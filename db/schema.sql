fa5ea92d2c99a99d8e819442ac40761c99c6f6cd
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-06-22 12:03:33 -0700

    created my tables for employees, role, and department, on seeds.sql i havev implemented the roles, ids, departments, and names

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employeesTracker_db;
+CREATE DATABASE employeesTracker_db;
+USE employeesTracker_db;
+
+CREATE TABLE departments
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+    );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    FOREIGN KEY(department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+    );
+
+ CREATE TABLE employee (
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL
+    );
---
+++ db/seeds.sql
@@ -0,0 +1,42 @@
+INSERT INTO departments (department_name)
+VALUES 
+('Executive Board'),
+('Marketing'),
+('Human Resources'),
+('Finance'),
+('Engineering'),
+('Information Technology'),
+('Customer Relations'),
+('Research and Development'),
+('Legal'),
+('Maintenance'),
+('Coding');
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Chief Executive Officer', 555000.00, 1),
+('Marketing Manager', 125000.00, 2),
+('HR Director', 189000.00, 3),
+('Finance Head', 145000.00, 4),
+('Senior Engineer', 185000.00, 5),
+('IT Manager', 125000.00, 6),
+('Customer Relations Manager', 75000.00, 7),
+('Research and Development Manager ', 185000.00, 8),
+('Legal Manager', 95000.00, 9),
+('Maintenance Manager', 135000.00, 10),
+('Front-End Developer', 70000, 11),
+('Front-End Developer Lead', 125000, 12);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Cesar', 'Alegria', 1, 1),
+('Guillermo', 'Perez', 2, 2),
+('Luis', 'Duran', 3, 3),
+('Donavan', 'Colton', 4, 4),
+('Diana', 'Alegria', 5, 5),
+('Selena', 'Perez', 6, 6),
+('John', 'Edralin', 7, 7),
+('Salia', 'Steward', 8, 8),
+('Gerilyn', 'Rufin', 9, 9),
+('Christian', 'Alvarez', 10, 10)
