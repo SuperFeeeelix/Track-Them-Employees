23910cec8151717136d441cc89ab856f4721768f
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-06-26 15:55:02 -0700

    added my switch statment to call in all functions for the user to add or delete

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const mysql = require("mysql2");
 // create a MySQL connection
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 3000,
+    port: 3306,
     user: "root",
-    password: "",
+    password: "Mexican$!9",
     database: "employeesTracker_db",
 });
 
@@ -42,18 +42,66 @@ inquirer.prompt({
 })
 
 // function to view all departments
+.then((answer) => {
+    switch (answer.action) {
+        case "view all departments":
+            // Call the function to handle view all departments
+            viewAllDepartments();
+            break;
+        case "View all roles":
+            // View all roles
+            viewAllRoles();
+            break;
+        case "View all Employees":
+            // View all employees
+                viewAllEmployees();
+                break;
+        case "Add a department":
+            // adds a department
+                addDepartment();
+                break;
+        case "Add a role":
+            //Adds a role 
+                addRole();
+                break;
+        case "Add an employee":
+            //Adds employee
+                addEmployee();
+                break;
+        case "Add a manager":
+            //Adds Manager
+                addManager();
+                break;
+        case "Update an employee role":
+            //Call the function to handle updating an employee's role
+                updateEmployeeRole();
+                break;
+        case "View employees by manager":
+            //Call the function to handle viewing employees by manager
+                viewEmployeesByManager();
+                break;
+        case "View employees by department":
+            //Call the function to handle viewing employess by department
+                viewEmployeesByDepartment();
+                break;
+        case "Delete departments, roles, or employees":
+            //Call the function to handle deleting departments, roles, or employees
+                deleteData();
+                break;
+        case "View the total utilized budget of a department":
+            //Call the function to handle viewing the total utilized budget
+                viewBudgetByDepartment();
+                break;
+        case "Exit":
+            // End the database connection and exit the application
+            connection.end();
+            console.log("Goodbye!!!!");
+            break;
+            default:
+            console.log("invalid choice. Please try again.");
+            start();
 
-// function to view all roles
-// function to view all employees
+    }
+});
 
-// function to add a department
 
-// Function to add an employee
-
-// Function to add a Manager
-
-// function to update an employee role
-
-// Function to View Employee By Manager
-
-// Function to view Employees by Department
